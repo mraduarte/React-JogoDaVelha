5becb788246f3949e39e53ccf76dc4c23cac188a
mraduarte <mcduarte9@hotmail.com>
2025-04-03 21:25:43 -0300

    Adicionada condição de vitória

--- jogo-da-velha/src/components/Board.tsx
+++ jogo-da-velha/src/components/Board.tsx
@@ -7,7 +7,7 @@ const Board = () => {
     Array(9).fill(null)
   );
   const [aiIsThinking, setAiIsThinking] = useState<boolean>(false);
-  const winner = false;
+  const winner = calculateWinner(squares);
 
   const handleClick = (i: number) => {
     if (squares[i] || winner || aiIsThinking) return;
@@ -50,9 +50,31 @@ const Board = () => {
         <Square value={squares[7]} onClick={() => handleClick(7)} />
         <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
-      <button className="reset-button" onClick={resetGame}>Reiniciar jogo</button>
+      <button className="reset-button" onClick={resetGame}>
+        Reiniciar jogo
+      </button>
     </div>
   );
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+      return squares[a];
+  }
 };
 
 export default Board;
